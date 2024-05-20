96f01298b378d644f887d75ee3f705d0e6701aed
Jagadeesh B Mothiki <jb.mothiki@gmail.com>
2024-05-20 17:19:01 +0530

    Mini-Max Sum

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-#!/bin/python3
-
 import math
 import os
 import random
@@ -8,28 +6,24 @@ import sys
 
 
 #
-# Complete the 'plusMinus' function below.
+# Complete the 'miniMaxSum' function below.
 #
-# The function accepts INTEGER_ARRAY arr as parameter.
+# The function accepts INTEGER_ARRAY ary as parameter.
 #
 
-def plusminus(arr):
-    positive = negative = zero = 0
-    for i in arr:
-        if i > 0:
-            positive += 1
-        elif i < 0:
-            negative += 1
-        elif i == 0:
-            zero += 1
-    print(f'{positive/n:.6f}')
-    print(f'{negative/n:.6f}')
-    print(f'{zero/n:.6f}')
+def minimax_sum(ary):
+    arr_sum = 0
+    min_val, max_val = ary[0], ary[0]
+    for num in ary:
+        arr_sum += num
+        if num < min_val:
+            min_val = num
+        elif num > max_val:
+            max_val = num
+    print(arr_sum - max_val, arr_sum - min_val)
 
 
 if __name__ == '__main__':
-    n = 6
+    arr = [1, 3, 5, 7, 9]
 
-    arr = [-4, 3, -9, 0, 4, 1]
-
-    plusminus(arr)
+    minimax_sum(arr)
