06f4c41aa4e1abb3c079d0b92444e225223f03e7
Jagadeesh B Mothiki <jb.mothiki@gmail.com>
2024-06-11 16:36:10 +0530

    CamelCase

--- main.py
+++ main.py
@@ -1,35 +1,22 @@
-#!/bin/python3
-
-import math
-import os
-import random
 import re
-import sys
 
 
-#
-# Complete the 'timeConversion' function below.
-#
-# The function is expected to return a STRING.
-# The function accepts STRING s as parameter.
-#
+def camel_case(name):
+    operation, method, word = name.split(';')
+    if operation == 'S':
+        list_of_words = re.sub(r"([A-Z])", r" \1", word).lower()
+        if method == "M":
+            return list_of_words.replace('()', '')
+        else:
+            return list_of_words
+    elif operation == 'C':
+        word_list = word.split()
+        camel_case_word = ''.join([word_list[0]] + [w.capitalize() for w in word_list[1:]])
+        if method == "M":
+            return camel_case_word.replace(' ', '') + "()"
+        else:
+            return camel_case_word.replace(' ', '')
 
-def time_conversion(s):
-    hours = int(s[:2].replace('12', '00'))
-    if s[-2:] == 'PM':
-        return str(hours + 12) + s[2:-2]
-    return f'{hours:02d}' + s[2:-2]
 
-
-if __name__ == '__main__':
-    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    s = '12:01:00PM'
-
-    result = time_conversion(s)
-
-    # fptr.write(result + '\n')
-
-    # fptr.close()
-
-    print(result)
+w = "S;C;OrangeHighlighter"
+print(camel_case(w))
